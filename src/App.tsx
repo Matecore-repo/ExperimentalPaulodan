180ccb3f57af78d4635ef50d993eed7444e6343e
iangelgit <nachoangelone@gmail.com>
2025-07-08 17:14:57 -0300

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero"
+import Electric from "./components/Electric"
+import Navbar from "./components/Navbar"
+import Gallery from "./components/Gallery"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <>
+        <Navbar />
+        <Hero />
+        <Electric />
+        <Gallery />
+        <Footer />
+    </>
+  )
+}
+
+export default App
